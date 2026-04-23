14f565fe793fc32916f506924d827e05b3344e62
KyriosAxell <soad.dvm@hotmail.com>
2026-04-22 18:31:55 -0600

    Implement a basic thread pool and update HTTP server to handle requests concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::fmt::format;
-use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+use test_voice::ThreadPool;
 
 fn simple_handle_connection(mut stream: TcpStream) {
     let status_line = "HTTP/1.1 200 OK";
@@ -16,35 +17,41 @@ fn simple_handle_connection(mut stream: TcpStream) {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader  = BufReader::new(&stream);
-    let request_line = buf_reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap();
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("src/hello.xhtml").unwrap();
-        let length = contents.len();
-        let response = format!(
-            "{status_line}\r\n\
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // Ejemplo
+    /*    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "src/hello.xhtml")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "src/404.xhtml")
+        };
+    */
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "src/hello.xhtml"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(4));
+            ("HTTP/1.1 200 OK", "src/hello.xhtml")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "src/404.xhtml"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n\
              Content-Length: {length}\r\n\r\n\
              {contents}"
-        );
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("src/404.xhtml").unwrap();
-        let length = contents.len();
-        
-    }
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(2) {
         let data = stream.unwrap();
-        handle_connection(data);
+        pool.execute(|| {
+            handle_connection(data);
+        });
     }
 }
